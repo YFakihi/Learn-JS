c9ceabe3748296122d9dbb33b799e40470ad714a
yfakihi <fakihiyassin01@gmail.com>
2024-04-25 17:28:30 +0100

    initaila commit

--- JavaScript_Array_Manipulation_Problems/3step.js
+++ JavaScript_Array_Manipulation_Problems/3step.js
@@ -8,3 +8,19 @@ function findeplucate(arr){
 const arra = findeplucate([2,8,17,2,-2,-3]);
 
  console.log(arra);
+
+
+
+//  function test(a){
+//     a=   a.filter(s=> s!=3)
+   
+//   a=  a.map(s => s*=s  )
+    
+//     let b= a.reduce((a,s)=> a+=s)
+    
+//     return b
+// }
+
+// const tr = [3,3,2,9]
+
+// console.log(test(tr))
